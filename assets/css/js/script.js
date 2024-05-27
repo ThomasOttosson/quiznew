04f513a4f8e47e79fcf9e9b7cb7c83461bab0e94
ThomasOttosson Ottosson <tomasottosson1@gmail.com>
2024-05-27 18:03:37 +0000

    added some more javascript to questions

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -3,6 +3,8 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+
 
 //if start quiz button clicked
 start_btn.onclick = ()=> {
@@ -12,4 +14,9 @@ start_btn.onclick = ()=> {
 //if Exit Button clicked
 exit_btn.onclick = ()=> {
     info_box.classList.remove("activeInfo"); //hide the info box
+}
+
+continue_btn.onclick = ()=> {
+    info_box.classList.remove("activeInfo"); //hide the info box
+    quiz_box.classList.add("activeQuiz"); //show the quiz box
 }
