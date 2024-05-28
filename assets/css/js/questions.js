34bc67bf9e09012553d08d1d0157e02d0b530aee
ThomasOttosson Ottosson <tomasottosson1@gmail.com>
2024-05-28 18:47:44 +0000

    add added more js

--- assets/css/js/questions.js
+++ assets/css/js/questions.js
@@ -1,71 +1,58 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
-  },
-    {
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
-  },
-    {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
-  },
-    {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
-    {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
-  },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  {
+  numb: 1,
+  question: "Which country won Eurovision Song Contest in 2024?",
+  answer: "Switzerland",
+  options: [
+    "Ukraine",
+    "Switzerland",
+    "UK",
+    "Sweden"
+  ]
+},
+  {
+  numb: 2,
+  question: "Which year was Michael Jackson born?",
+  answer: "1958",
+  options: [
+    "1955",
+    "1953",
+    "1958",
+    "1959"
+  ]
+},
+  {
+  numb: 3,
+  question: "Which artist has won the most Grammy Awards?",
+  answer: "Beyoncé",
+  options: [
+    "Beyoncé",
+    "Phil Collins",
+    "Michael Jackson",
+    "Paul McCartney"
+  ]
+},
+  {
+  numb: 4,
+  question: "Which person was a member in The Beatles?",
+  answer: "John Lennon",
+  options: [
+    "John Lennon",
+    "Bruce Springsteen",
+    "Nigel Farage",
+    "Billy Idol"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which artist has a song called Easy Lover?",
+  answer: "Phil Collins",
+  options: [
+    "Billy Idol",
+    "Adele",
+    "Rod Stewart",
+    "Phil Collins"
+  ]
+}
 ];
