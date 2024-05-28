462bf158f730c7182038b33af6cbac6843fafa5e
ThomasOttosson Ottosson <tomasottosson1@gmail.com>
2024-05-28 14:33:40 +0000

    add added more javascript

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -5,6 +5,7 @@ const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 
+const option_list = document.querySelector("option_list");
 
 //if start quiz button clicked
 start_btn.onclick = ()=> {
@@ -43,7 +44,6 @@ next_btn.onclick = ()=> {
 //getting questions and options from array
 function showQuestions(index) {
     const que_text = document.querySelector(".que_text");
-    const option_list = document.querySelector("option_list");
     let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].questions +'</span>';
     let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
                      +  '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
@@ -60,15 +60,28 @@ function showQuestions(index) {
 function optionSelected(answer) {
     let userAns = answer.textContent;
     let correctAns = questions[que_count].answer;
+    let allOptions = option_list.children.length;
     if(userAns == correctAns) {
     answer.classList.add("correct");
     console.log("Answer is Correct");
 }else{
     answer.classList.add("incorrect");
     console.log("Answer is Wrong");
-}
+
+    //if answers is incorrect then automatically selected the correct answer
+    for (let i = 0; i < allOptions; i++) {
+        if(option_list.children[i].textContent == correctAns) {
+            option_list.children[i].setAttribute("class", "option correct")
+        }
+    }
 }
 
+//once user selected disabled all options
+for (let i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled");
+
+}
+}
 
 
 
